514865ebb80377c997ae2df570393f54b83e6e46
Luis Weich <luis.gabriel.weich@gmail.com>
2026-01-09 23:54:27 +0100

    updates

--- common/src/main/java/template/quarkus/common/util/Blocker.java
+++ common/src/main/java/template/quarkus/common/util/Blocker.java
@@ -1,14 +1,16 @@
 package template.quarkus.common.util;
 
-import java.util.concurrent.TimeUnit;
-
 import jakarta.enterprise.context.ApplicationScoped;
 
 import io.quarkus.vertx.ConsumeEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import template.quarkus.common.Events;
 
 @ApplicationScoped
 public class Blocker {
+
+    private static final Logger log = LoggerFactory.getLogger(Blocker.class);
 
     private boolean enabled = true;
 
@@ -17,27 +19,19 @@ public class Blocker {
     @ConsumeEvent(Events.ALIVE_NAME)
     public void consume(String value) {
         if (Events.ALIVE_DOWN.equals(value)) {
-            setEnabled(false);
+            enabled = false;
         } else if (Events.ALIVE_UP.equals(value)) {
-            setEnabled(true);
+            enabled = true;
         }
     }
 
-    public void blockIfDown() {}
-
-    public void block(long amount, TimeUnit timeUnit) {
-        try {
-            Thread.sleep(timeUnit.toMillis(amount));
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+    public void blockIfDown() {
+        while (!enabled) {
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                log.error("Interrupted", e);
+            }
         }
-    }
-
-    public void block() {
-        block(5, TimeUnit.SECONDS);
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
     }
 }
