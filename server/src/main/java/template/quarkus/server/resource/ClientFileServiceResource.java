f2ba2b5da4d4ac9e95119425244839bb36c5b0b7
ergosumcogito <ergosumcogito@users.noreply.github.com>
2026-01-09 13:30:12 +0100

    Add template code for auto-registering client on 1st call

--- server/src/main/java/template/quarkus/server/resource/ClientFileServiceResource.java
+++ server/src/main/java/template/quarkus/server/resource/ClientFileServiceResource.java
@@ -5,6 +5,7 @@ import jakarta.inject.Inject;
 import template.quarkus.common.ClientFileService;
 import template.quarkus.common.FileContent;
 import template.quarkus.common.UpdatePackage;
+import template.quarkus.server.service.ClientRegistry;
 import template.quarkus.server.service.FileService;
 
 public class ClientFileServiceResource implements ClientFileService {
@@ -12,15 +13,36 @@ public class ClientFileServiceResource implements ClientFileService {
     @Inject
     private FileService fileService;
 
+    @Inject
+    private ClientRegistry clientRegistry;
+
     public ClientFileServiceResource() {}
 
     @Override
     public void write(UpdatePackage updatePackage) {
+        // Auto-register client on first call
+        // registerClientIfNeeded(updatePackage);
+
         fileService.writeThrough(updatePackage);
     }
 
     @Override
     public FileContent read(String file) {
+        // Auto-register client on first call
+        // registerClientIfNeeded(updatePackage);
+
         return new FileContent(fileService.read(file));
     }
+
+
+    private void registerClientIfNeeded(UpdatePackage updatePackage) {
+//        String clientId = updatePackage.getClientId();
+//        String clientIp = updatePackage.getClientIp();
+//        int clientPort = updatePackage.getClientPort();
+//
+//        if (!clientRegistry.getClients().containsKey(clientId)) {
+//            clientRegistry.registerClient(clientId, clientIp, clientPort);
+//        }
+    }
+
 }
