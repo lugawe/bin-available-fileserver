e9496558d199904ddbc3c14703e6a6c29c78a9c4
Luis Weich <luis.gabriel.weich@gmail.com>
2026-01-10 15:20:49 +0100

    update Events

--- common/src/main/java/template/quarkus/common/Events.java
+++ common/src/main/java/template/quarkus/common/Events.java
@@ -3,8 +3,12 @@ package template.quarkus.common;
 public final class Events {
 
     public static final String ALIVE_NAME = "alive";
-    public static final String ALIVE_DOWN = "down";
-    public static final String ALIVE_UP = "up";
+    public static final String ALIVE_DOWN = "alive-down";
+    public static final String ALIVE_UP = "alive-up";
+
+    public static final String NODE_NAME = "node";
+    public static final String NODE_DOWN = "node-down:";
+    public static final String NODE_UP = "node-up:";
 
     private Events() {}
 }
