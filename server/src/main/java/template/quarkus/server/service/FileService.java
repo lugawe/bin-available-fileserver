aaaa1e625c92d0cadf24b863514dc9a0ad7cef5b
Luis Weich <luis.gabriel.weich@gmail.com>
2026-01-10 15:09:53 +0100

    update FileService

--- server/src/main/java/template/quarkus/server/service/FileService.java
+++ server/src/main/java/template/quarkus/server/service/FileService.java
@@ -1,15 +1,11 @@
 package template.quarkus.server.service;
 
-import java.util.Collection;
-
-import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import template.quarkus.common.UpdatePackage;
-import template.quarkus.common.sync.SyncFileService;
 import template.quarkus.common.sync.SyncFileServiceRegistry;
 
 @ApplicationScoped
@@ -23,14 +19,7 @@ public class FileService {
     @Inject
     private SyncFileServiceRegistry syncFileServiceRegistry;
 
-    private Collection<SyncFileService> syncFileServiceReplicas;
-
     public FileService() {}
-
-    @PostConstruct
-    public void init() {
-        syncFileServiceReplicas = syncFileServiceRegistry.getAllRegistered();
-    }
 
     public void store(UpdatePackage updatePackage) {
         fileStorage.write(updatePackage.getFiles());
@@ -38,7 +27,8 @@ public class FileService {
 
     public void writeThrough(UpdatePackage updatePackage) {
         fileStorage.write(updatePackage.getFiles());
-        syncFileServiceReplicas.parallelStream().forEach(fs -> fs.sync(updatePackage));
+
+        syncFileServiceRegistry.getAllRegistered().parallelStream().forEach(fs -> fs.sync(updatePackage));
     }
 
     public byte[] read(String file) {
