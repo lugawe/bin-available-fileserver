106350e8249511c4c659d5da77bcb4e44eea28b8
Luis Weich <luis.gabriel.weich@gmail.com>
2026-01-10 15:13:41 +0100

    updates 2

--- server/src/main/java/template/quarkus/server/service/ScheduledPingService.java
+++ server/src/main/java/template/quarkus/server/service/ScheduledPingService.java
@@ -8,11 +8,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import template.quarkus.common.ping.PingService;
 import template.quarkus.common.ping.PingServiceRegistry;
+import template.quarkus.common.util.Blocker;
 
 @ApplicationScoped
 public class ScheduledPingService {
 
+    // TODO events for node up/down and master up/down
+
     private static final Logger log = LoggerFactory.getLogger(ScheduledPingService.class);
+
+    @Inject
+    private Blocker blocker;
 
     @Inject
     private PingServiceRegistry pingServiceRegistry;
@@ -21,15 +27,15 @@ public class ScheduledPingService {
 
     @Scheduled(every = "3s")
     public void scheduledPinging() {
-        PingService.PingPackage pingPackage = new PingService.PingPackage("ping");
-        pingServiceRegistry.getAllRegisteredMap().forEach((k, v) -> {
-            try {
-                PingService.PingPackage p = v.ping(pingPackage);
-                log.info("Node {} is available: {}", k, p);
-                // TODO Besseres wann ist Node nicht verfügbar
-            } catch (Exception e) {
-                log.warn("Node {} is not available", k);
-            }
+        blocker.maybeSendMessage(() -> {
+            PingService.PingPackage pingPackage = new PingService.PingPackage("ping");
+            pingServiceRegistry.getAllRegisteredMap().forEach((k, v) -> {
+                try {
+                    v.ping(pingPackage);
+                } catch (Exception e) {
+                    log.warn("Node {} is not available", k);
+                }
+            });
         });
     }
 }
