f2e2ce1ad8927ee5f67c6d3e49b0f53340d81c4c
Luis Weich <luis.gabriel.weich@gmail.com>
2026-01-11 02:20:48 +0100

    updates 3

--- server/src/main/java/template/quarkus/server/service/Storage.java
+++ server/src/main/java/template/quarkus/server/service/Storage.java
@@ -1,31 +1,38 @@
 package template.quarkus.server.service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import template.quarkus.common.content.ChangeEntry;
+import template.quarkus.common.content.FileEntry;
 
 @ApplicationScoped
 public class Storage {
 
     private static final Logger log = LoggerFactory.getLogger(Storage.class);
 
-    private final List<ChangeEntry> files = new ArrayList<>();
+    private final SortedSet<ChangeEntry> changeEntries =
+            new TreeSet<>(Comparator.comparingInt(ChangeEntry::version).reversed());
 
     public Storage() {}
 
     public void write(ChangeEntry changeEntry) {
-        files.add(changeEntry);
         log.info("Write");
+        changeEntries.add(changeEntry);
     }
 
     public byte[] read(String file) {
-        // byte[] bytes = store.get(file);
         log.info("Read");
+        for (ChangeEntry entry : changeEntries) {
+            Optional<FileEntry> fileEntry =
+                    entry.files().stream().filter(e -> e.name().equals(file)).findFirst();
+            if (fileEntry.isPresent()) {
+                return fileEntry.get().bytes();
+            }
+        }
         return null;
     }
 }
