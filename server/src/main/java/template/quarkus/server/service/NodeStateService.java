362d14d26d846362ba8a503addbad4ba12c3f0ff
Luis Weich <luis.gabriel.weich@gmail.com>
2026-01-11 19:47:04 +0100

    add NodeStateService

---
+++ server/src/main/java/template/quarkus/server/service/NodeStateService.java
@@ -0,0 +1,66 @@
+package template.quarkus.server.service;
+
+import java.util.*;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import io.quarkus.scheduler.Scheduled;
+import io.vertx.core.eventbus.EventBus;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import template.quarkus.common.Events;
+import template.quarkus.common.ping.PingService;
+import template.quarkus.common.ping.PingServiceRegistry;
+
+@ApplicationScoped
+public class NodeStateService {
+
+    private final Set<String> activeNodes = new LinkedHashSet<>();
+
+    @ConfigProperty(name = "node.replicas")
+    private List<String> replicas;
+
+    @Inject
+    private EventBus eventBus;
+
+    @Inject
+    private PingServiceRegistry pingServiceRegistry;
+
+    public NodeStateService() {}
+
+    @PostConstruct
+    public void init() {
+        activeNodes.addAll(replicas);
+    }
+
+    @Scheduled(every = "3s")
+    public void ping() {
+
+        // TODO message loss
+
+        PingService.PingPackage pingPackage = new PingService.PingPackage("ping");
+
+        for (Map.Entry<String, PingService> entry :
+                pingServiceRegistry.getAllRegisteredMap().entrySet()) {
+
+            String nodeId = entry.getKey();
+            PingService pingService = entry.getValue();
+
+            try {
+                pingService.ping(pingPackage);
+                if (activeNodes.add(nodeId)) {
+                    eventBus.publish(Events.NODE_UP, nodeId);
+                }
+            } catch (Exception e) {
+                if (activeNodes.remove(nodeId)) {
+                    eventBus.publish(Events.NODE_DOWN, nodeId);
+                }
+            }
+        }
+    }
+
+    public Set<String> getActiveNodes() {
+        return Collections.unmodifiableSet(activeNodes);
+    }
+}
