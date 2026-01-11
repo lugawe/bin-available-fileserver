ca572a66cd2b161b22afb57d8c96f98333bee1f0
Luis Weich <luis.gabriel.weich@gmail.com>
2026-01-12 00:28:07 +0100

    update client

---
+++ client/src/main/java/template/quarkus/client/ClientRESTClient.java
@@ -0,0 +1,28 @@
+package template.quarkus.client;
+
+import java.net.URI;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
+import template.quarkus.common.ClientFileRESTService;
+
+@ApplicationScoped
+public class ClientRESTClient {
+
+    @ConfigProperty(name = "client.endpoint")
+    private String endpoint;
+
+    public ClientRESTClient() {}
+
+    public ClientFileRESTService fileService(String nodeId) {
+        return RestClientBuilder.newBuilder()
+                .baseUri(URI.create("http://" + nodeId + ":8080/api"))
+                .build(ClientFileRESTService.class);
+    }
+
+    public ClientFileRESTService fileService() {
+        return fileService(endpoint);
+    }
+}
